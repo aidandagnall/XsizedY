3accb4bbe40111a90d357b435909fab9621969ca
unknown <tribsmc@gmail.com>
2020-11-28 11:52:29 +0000

    added commands

--- example_bot.py
+++ example_bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+import csv
 import random
+import config
 import os
 client = commands.Bot(command_prefix = '!')
 
@@ -16,5 +18,14 @@ async def on_member_join(ctx,member):
 async def on_member_remove(ctx,member):
     await ctx.send(f'(member) has left the server')
 
+@client.command()
+async def random(ctx):
+    break
 
+@client.command()
+async def choice(ctx):
+    break
+
+async def fixedNumber(ctx,number,animal):
+    break
 client.run(config.bot_token)
